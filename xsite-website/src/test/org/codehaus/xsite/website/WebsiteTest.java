eeb03f66101199b2c463351a5972689553a265e2
mauro <mauro@cd184976-2b07-0410-8b60-c68cbc61095d>
2006-02-25 16:02:37 +0000

    Added m2 support. Added xerces dependency for m1 - needed for classloading issues.
    git-svn-id: http://svn.codehaus.org/xsite/trunk@25 cd184976-2b07-0410-8b60-c68cbc61095d

--- xsite-website/src/test/org/codehaus/xsite/website/WebsiteTest.java
+++ xsite-website/src/test/org/codehaus/xsite/website/WebsiteTest.java
@@ -20,14 +20,14 @@ public class WebsiteTest extends TestCase {
     protected void setTestDir() {
         testSrcDir =  System.getProperty("test.src.dir");
         if ( testSrcDir == null ){
-            testSrcDir = "xsite-website/src/"; 
+            testSrcDir = "xsite-website/src/test/"; 
         } else if ( !testSrcDir.endsWith(File.separator) ){
             testSrcDir = testSrcDir + File.separator; 
         }        
     }
     
     public void testBuild() throws IOException{
-        Main.main(new String[] {testSrcDir+"content/website.xml", testSrcDir+"templates/skin.html", "target/xsite"});
+        Main.main(new String[] {testSrcDir+"../content/website.xml", testSrcDir+"../templates/skin.html", "target/xsite"});
     }
 
 }
