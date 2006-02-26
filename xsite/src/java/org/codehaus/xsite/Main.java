c616ffdd25f52c387b2c803ae6d5b63372602c95
mauro <mauro@cd184976-2b07-0410-8b60-c68cbc61095d>
2006-02-26 12:05:33 +0000

    Moved instantiation of XSite to separate method. Fixed message typo.
    git-svn-id: http://svn.codehaus.org/xsite/trunk@33 cd184976-2b07-0410-8b60-c68cbc61095d

--- xsite/src/java/org/codehaus/xsite/Main.java
+++ xsite/src/java/org/codehaus/xsite/Main.java
@@ -12,7 +12,7 @@ import java.net.URL;
 
 
 /**
- * Command line entry point for building org.codehaus.xsite.
+ * Command line entry point for building XSite.
  *
  * @author Joe Walnes
  * @author J&ouml;rg Schaible
@@ -25,30 +25,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
             System.err.println("Usage: java " + XSite.class.getName()
-                    + " <path-to-org.codehaus.xsite.xml> <path-to-skin> <output-dir>");
+                    + " <path-to-sitemap> <path-to-skin> <output-dir>");
             System.exit(-1);
         }
 
-        final URL url;
+        final URL xsiteURL;
         File xsiteFile = new File(XSITE_FILE);
         if (xsiteFile.exists()) {
-            url = xsiteFile.toURL();
+            xsiteURL = xsiteFile.toURL();
         } else {
-            url = Main.class.getResource(XSITE_FILE);
+            xsiteURL = Main.class.getResource(XSITE_FILE);
         }
 
-        if (url == null) {
+        if (xsiteURL == null) {
             System.err.println("Could not find " + XSITE_FILE);
             System.exit(-1);
         }
 
+        XSite xsite = instantiateXSite(xsiteURL);
+        xsite.build(new File(args[0]), new File(args[1]), new File(args[2]));
+    }
+
+    private static XSite instantiateXSite(URL url) {
         ScriptedContainerBuilder builder = new XMLContainerBuilder(url, Main.class.getClassLoader());
         ObjectReference reference = new SimpleReference();
         builder.buildContainer(reference, null, null, false);
         PicoContainer pico = (PicoContainer)reference.get();
-
-        XSite xsite = (XSite)pico.getComponentInstanceOfType(XSite.class);
-        xsite.build(new File(args[0]), new File(args[1]), new File(args[2]));
+        return (XSite)pico.getComponentInstanceOfType(XSite.class);        
     }
 
 }
