e0606c1722e5e5fb192045a8c584154b5853072f
mauro <mauro@cd184976-2b07-0410-8b60-c68cbc61095d>
2006-10-25 19:50:08 +0000

    Removed unpacked classes from bin distribution. Refactored scripts to build classpath dynamically.
    git-svn-id: http://svn.codehaus.org/xsite/trunk@69 cd184976-2b07-0410-8b60-c68cbc61095d

--- xsite-distribution/src/bin/xsite.bat
+++ xsite-distribution/src/bin/xsite.bat
@@ -1,5 +1,8 @@
 rem run XSite
 
-EXEC="%JAVA_HOME%\bin\java -classpath %XSITE_HOME%\classes org.codehaus.xsite.Main $@"
+set XSITE_CLASSPATH=
+for %%path in (%XSITE_HOME%\lib\*.jar) do call cpappend.bat %%path
+
+EXEC="%JAVA_HOME%\bin\java -classpath %XSITE_CLASSPATH% org.codehaus.xsite.Main $@"
 %EXEC%
 
